ed9cee7ddd47144e40eba571524a76872acb32b3
Ahmed Harbi <ahmed.harbi.eg@gmail.com>
2021-11-23 04:07:48 +0200

    CLearn

---
+++ Application 1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Application 1
+//
+//  Created by Ahmed Harbi on 17/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Play Ground/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Play Ground
+//
+//  Created by Ahmed Harbi on 17/11/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
